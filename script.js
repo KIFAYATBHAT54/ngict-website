5a203aec363177596f5b147330effcd712a4ba53
KIFAYATBHAT54 <kifayatbhat54@gmail.com>
2026-09-19 12:11:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -625,17 +625,4 @@ document.addEventListener("keydown", function(event) {
     }
 
 });
-document.addEventListener("keydown", function(event) {
-
-    if (event.key === "Escape") {
-
-        document.querySelectorAll(".notice-modal").forEach(function(modal) {
-            modal.classList.remove("show");
-        });
-
-        document.body.style.overflow = "";
-    }
-
-});
-
 
